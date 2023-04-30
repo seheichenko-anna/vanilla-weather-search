fe62f4a264e3324f09accd70632f4d684b993560
seheichenko-anna <seheichenko.anna@gmail.com>
2023-04-30 15:24:54 +0300

    Add new API key

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -90,7 +90,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
